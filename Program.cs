b996714360d9133485ae44bf18a50187e666dbf2
Matheus <matheus.lascala02@gmail.com>
2021-12-10 20:12:14 -0300

    Inicio projeto

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/* Editar arquivo de projeto -> botão direito no projeto Minimal_APis.Net6
+ *
+ *<Project Sdk="Microsoft.NET.Sdk"> mudar para <Project Sdk="Microsoft.NET.Sdk.Web">
+ *
+ * Informa que sera uma aplicação Web
+ *
+ * <OutputType>Exe</OutputType> Remover pois não sera usado
+ *
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+// Services 
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Ola usuario" );
+
+app.Run();
